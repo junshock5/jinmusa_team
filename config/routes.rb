74c90fc973a0e9b71a33aaf0a714eb2760880fe3
Ubuntu <ubuntu@ip-172-31-86-161.ec2.internal>
2020-04-04 06:39:36 +0000

    라우트 설정 , application.html 구조 변경

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :posts
+  devise_for :users
   get 'grayscale/index'
+  
+  
+  get 'posts/index'
+  get 'posts/new'
+  
+  
+  root 'grayscale#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
