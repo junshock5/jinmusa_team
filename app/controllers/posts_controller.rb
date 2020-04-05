cf4bba73beaefe2c9711a344a683553d912e4326
Junseok <topojs8@likelion.org>
2020-04-05 17:22:13 +0000

    LogIn Page add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :update, :destroy]
+  
 
   # GET /posts
   # GET /posts.json
