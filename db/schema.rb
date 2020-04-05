12777b2a0b7f68d6e78fb9b8582ccb966aa0ef1d
Junseok <topojs8@likelion.org>
2020-04-05 19:03:10 +0000

    카카오 지도 api 연동 및 rails_db devise 추가

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_063042) do
+ActiveRecord::Schema.define(version: 2020_04_06_132435) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "content"
+    t.float "lat"
+    t.float "lng"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
