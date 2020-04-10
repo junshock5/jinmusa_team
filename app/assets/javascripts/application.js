a3477b148df1c68b467eb0b2916ee1e0db36a7c1
Junseok <topojs8@likelion.org>
2020-04-10 17:01:05 +0000

    javascript를 이용한 문의하기(쪽지기능 구현)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,24 @@
 //
 //= require rails-ujs
 //= require activestorage
+
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+ 
+(function() {
+  $(document).on('click', '.toggle-window', function(e) {
+    e.preventDefault();
+    var panel = $(this).parent().parent();
+    var messages_list = panel.find('.messages-list');
+ 
+    panel.find('.panel-body').toggle();
+    panel.attr('class', 'panel panel-default');
+ 
+    if (panel.find('.panel-body').is(':visible')) {
+      var height = messages_list[0].scrollHeight;
+      messages_list.scrollTop(height);
+    }
+  });
+})();
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,29 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+
+
+
+  resources :conversations, only: [:create] do
+    member do
+        post :close
+    end
+    resources :messages, only: [:create]
+  end
+
+  get 'homes/index'
   get 'grayscale/index'
   
   
   get 'posts/index'
   get 'posts/new'
   
+  get 'messages/create'
+  get 'conversations/create'
+  
+  get 'conversations/conversated'
   
   root 'grayscale#index'
+  #root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
