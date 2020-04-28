e63b4bb2da60c868342c2e4d8cbc5338ff1eb451
JunSeokOh <topojs8@likelion.org>
2020-04-28 19:05:18 +0000

    문의하기 로그인페이지연결, common.css 파일제거

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,15 @@
 class HomesController < ApplicationController
   def index
-    session[:conversations] ||= []
- 
-    @users = User.all.where.not(id: current_user)
-    @conversations = Conversation.all
+
+    
+    if user_signed_in?
+       session[:conversations] ||= []
+       @users = User.all.where.not(id: current_user)
+       @conversations = Conversation.all
+    else
+      redirect_to controller: 'users', action: 'sign_in', params_name: "test"
+    end
+    
     #@conversations = Conversation.includes(:recipient, :messages)
                                 # .find(session[:conversations])
   end
